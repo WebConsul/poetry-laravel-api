eebd95995a3f56ff1c2827eaafa449b4148b5e5f
AlexStroganov <me@alexstroganov.ru>
2022-09-21 14:22:00 +0500

    feat: create starting migration files

---
+++ database/migrations/2022_09_20_225131_create_poets_data_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('poets_data', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('poet_id')
+                ->constrained('poets')
+                ->cascadeOnDelete();
+
+            $table->string('language', 5);
+
+            $table->string('first_name', 45)->nullable();
+            $table->string('last_name', 45)->nullable();
+
+            $table->string('description')->nullable();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('poets_data');
+    }
+};
