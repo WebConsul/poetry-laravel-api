fa8ae6015f3bc28cf7c57290faedbe6606f376da
erbolat_bit <saerbolat@gmail.com>
2022-09-21 08:25:07 +0500

    LEARN-POEMS-T-7: created models Collection, Line, Poem, Poet, PoetData and factories for them

---
+++ app/Models/PoetData.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class PoetData extends Model
+{
+    use HasFactory;
+
+    /**
+     * @var string
+     */
+    protected $table = 'poets_data';
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'language',
+        'first_name',
+        'last_name',
+        'description',
+        'poet_id',
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function poet(): BelongsTo
+    {
+        return $this->belongsTo(Poet::class);
+    }
+}
