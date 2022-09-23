3a8f1152ddff363eec49cc0b2588f59bc84b24ad
AlexStroganov <me@alexstroganov.ru>
2022-09-23 12:40:39 +0500

    LEARN-POEMS-T-21 Init ide-helper

--- app/Models/Poem.php
+++ app/Models/Poem.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
+/**
+ * @mixin IdeHelperPoem
+ */
 class Poem extends Model
 {
     use HasFactory;
--- app/Models/Poet.php
+++ app/Models/Poet.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * @mixin IdeHelperPoet
+ */
 class Poet extends Model
 {
     use HasFactory;
