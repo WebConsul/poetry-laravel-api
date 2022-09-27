4653ed87a545edc90fec66b8d59c04c00ae14716
Denis Kulagin <denis.kulagin1593@mail.ru>
2022-09-27 22:38:47 +0700

    Implementation get_poets

--- app/Http/Controllers/PoetController.php
+++ app/Http/Controllers/PoetController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Poet;
+use Illuminate\Http\JsonResponse;
 
+/**
+ * @OA\Get(
+ *     path="/api/poets",
+ *     tags={"poets"},
+ *     summary="Get all poets",
+ *     description="Getting all poets with pagination.",
+ *     operationId="getPoets",
+ *     @OA\Response(
+ *         response=200,
+ *         description="successful operation",
+ *     ),
+ *     @OA\RequestBody(
+ *         required=true,
+ *     )
+ * )
+ */
 class PoetController extends Controller
 {
-    //
+    public function get_poets(): JsonResponse
+    {
+        $poets = Poet::with('poetData')
+            ->paginate(1)
+            ->makeHidden(['created_at', 'updated_at']);
+
+        return response()->json(['poets' => $poets]);
+    }
 }
