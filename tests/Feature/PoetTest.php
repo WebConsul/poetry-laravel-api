b8c284fb8fd8afb216165c4bc040fda860afb4c2
Denis Kulagin <denis.kulagin1593@mail.ru>
2022-09-27 21:59:14 +0700

    Implementation test_get_poets

--- tests/Feature/PoetTest.php
+++ tests/Feature/PoetTest.php
@@ -3,20 +3,60 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
 class PoetTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_get_poets(): void
+    {
+        $this->seed();
+
+        $response = $this->get('/api/poets');
+
+        $response
+            ->assertHeader('content-type', 'application/json')
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'poets' => [
+                    '*' => [
+                        'id',
+                        'birth_date',
+                        'death_date',
+                        'portrait_url',
+                        'poet_data' => [
+                            '*' => [
+                                'id',
+                                'poet_id',
+                                'language',
+                                'first_name',
+                                'last_name',
+                                'description',
+                            ]
+                        ]
+                    ]
+                ]
+            ])
+            ->assertJson(fn(AssertableJson $json) => $json
+                ->whereAllType([
+                    'poets' => 'array',
+
+                    'poets.0.id' => 'integer',
+                    'poets.0.birth_date' => 'string',
+                    'poets.0.death_date' => 'string',
+                    'poets.0.portrait_url' => 'string',
+
+                    'poets.0.poet_data' => 'array',
+
+                    'poets.0.poet_data.0.id' => 'integer',
+                    'poets.0.poet_data.0.poet_id' => 'integer',
+                    'poets.0.poet_data.0.language' => 'string',
+                    'poets.0.poet_data.0.first_name' => 'string',
+                    'poets.0.poet_data.0.last_name' => 'string',
+                    'poets.0.poet_data.0.description' => 'string',
+                ])
+            );
     }
 }
