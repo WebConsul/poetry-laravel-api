df292ff78375ca6d748124f8e3026bcd58d022e7
Denis Kulagin <denis.kulagin1593@mail.ru>
2022-10-01 12:10:28 +0700

    Implementation PoetRequest

--- app/Http/Requests/PoetRequest.php
+++ app/Http/Requests/PoetRequest.php
@@ -6,14 +6,16 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class PoetRequest extends FormRequest
 {
+    protected $stopOnFirstFailure = true;
+
     /**
      * Determine if the user is authorized to make this request.
      *
      * @return bool
      */
-    public function authorize()
+    public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -21,10 +23,22 @@ class PoetRequest extends FormRequest
      *
      * @return array<string, mixed>
      */
-    public function rules()
+    public function rules(): array
+    {
+        $max = \DB::table('poets')->count();
+
+        return [
+            'per_page' => "nullable|numeric|integer|between:1,$max",
+            'page' => "nullable|numeric|integer|between:1,$max",
+        ];
+    }
+
+    public function messages(): array
     {
         return [
-            //
+            'numeric' => 'The :attribute is not a number.',
+            'integer' => 'The :attribute is not an integer',
+            'between' => 'The :attribute value :input is not between :min - :max.',
         ];
     }
 }
