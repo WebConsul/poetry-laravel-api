fe3ee2bcf682abab57f4e7a7b4660ce9083a766b
Dementrum <dmi56756877@yandex.ru>
2022-10-08 08:50:10 +0900

    LEARN-POEMS-T-32/Added backend-test,but lines commented out due to error

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -1,24 +1,23 @@
 <?php
 
-//
-//namespace Tests\Feature;
-//
-//use App\Models\User;
-//use Illuminate\Foundation\Testing\RefreshDatabase;
-//use Laravel\Sanctum\Sanctum;
-//use Tests\TestCase;
-//
-//class AuthTest extends TestCase
-//{
-//    use RefreshDatabase;
-//
-//    public function test_successful_response_over_secure_route(): void
-//    {
-//        $user = User::factory()->create();
-//        Sanctum::actingAs($user);
-//
-//        $response = $this->getJson('/api/user/me');
-//
-//        $response->assertOk();
-//    }
-//}
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Sanctum\Sanctum;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_successful_response_over_secure_route(): void
+    {
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
+
+        $response = $this->getJson('/api/user/me');
+
+        $response->assertOk();
+    }
+}
