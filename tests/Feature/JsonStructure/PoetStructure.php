12efb59220e4f6ca9868647e7e7d2c4e18d3f1b6
erbolat_bit <saerbolat@gmail.com>
2022-10-12 11:48:54 +0500

    add tests for get poet endpoint

--- tests/Feature/JsonStructure/PoetStructure.php
+++ tests/Feature/JsonStructure/PoetStructure.php
@@ -23,4 +23,49 @@ class PoetStructure
             ],
         ];
     }
+
+    /**
+     * @return array
+     */
+    public static function getPoetStructureWithPoemsAndCollections(): array
+    {
+        return [
+            'id',
+            'birth_date',
+            'death_date',
+            'portrait_url',
+            'slug',
+            'poet_data' => [
+                '*' => [
+                    'id',
+                    'poet_id',
+                    'language',
+                    'first_name',
+                    'last_name',
+                    'description',
+                ],
+            ],
+            'poems' => [
+                '*' => [
+                    'id',
+                    'poet_id',
+                    'translation_of',
+                    'language',
+                    'title',
+                    'created',
+                    'slug'
+                ]
+            ],
+            'collections' => [
+                '*' => [
+                    'id',
+                    'title',
+                    'publisher',
+                    'location',
+                    'date',
+                    'slug',
+                ],
+            ],
+        ];
+    }
 }
--- tests/Feature/PoetTest.php
+++ tests/Feature/PoetTest.php
@@ -2,10 +2,15 @@
 
 namespace Tests\Feature;
 
+use App\Models\Collection;
+use App\Models\Poet;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\Feature\JsonStructure\PaginatedList;
 use Tests\Feature\JsonStructure\PaginatedPoetList;
+use Tests\Feature\JsonStructure\PoetStructure;
 use Tests\TestCase;
 
 class PoetTest extends TestCase
@@ -66,5 +71,37 @@ class PoetTest extends TestCase
                     ]
                 )
             );
+    }
+
+    public function test_get_poet_by_slug_with_correct_slug(): void
+    {
+        $slug = Str::slug('Poet firstname lastname');
+        $poet = Poet::factory()
+            ->hasPoetData(3)
+            ->hasPoems(3)
+            ->create([
+                'slug' => $slug
+            ]);
+
+        $poet->collections()->sync(Collection::factory()->create()->id);
+
+        $response = $this->getJson(route('poet.show', ['slug' => $slug]));
+        $response->assertHeader('content-type', 'application/json')
+            ->assertStatus(200)
+            ->assertJsonStructure(['data' => PoetStructure::getPoetStructureWithPoemsAndCollections()])
+            ->assertJsonCount(3, 'data.poet_data')
+            ->assertJsonCount(3, 'data.poems');
+
+        $this->assertEquals($slug, Arr::get($response->decodeResponseJson(), 'data.slug'));
+    }
+
+    public function test_get_poet_by_slug_without_correct_slug(): void
+    {
+        $slug = Str::slug('Poet firstname lastname');
+
+        $this->getJson(route('poet.show', ['slug' => $slug]))
+            ->assertHeader('content-type', 'application/json')
+            ->assertNotFound();
+
     }
 }
