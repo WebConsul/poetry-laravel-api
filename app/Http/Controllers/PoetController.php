2cbd6d39e2f639693679fdc358f07ab285137641
erbolat_bit <saerbolat@gmail.com>
2022-10-12 10:32:00 +0500

    add api documentation

--- app/Http/Controllers/PoetController.php
+++ app/Http/Controllers/PoetController.php
@@ -7,29 +7,28 @@ use App\Http\Requests\PoetRequest;
 use App\Http\Resources\PoetResource;
 use App\Models\Poet;
 use Illuminate\Http\JsonResponse;
-use OpenApi\Annotations as OA;
 
-/**
- * @OA\Get(
- *     path="/api/poets",
- *     tags={"poets"},
- *     summary="Get all poets",
- *     description="Getting all poets with pagination.",
- *     operationId="getPoets",
- *     @OA\Response(
- *         response=200,
- *         description="successful operation",
- *     ),
- *     @OA\Parameter(
- *         name="per_page",
- *         in="query",
- *         required=false,
- *         @OA\Schema(type="integer")
- *     )
- * )
- */
 class PoetController extends Controller
 {
+    /**
+     * @OA\Get(
+     *     path="/api/poets",
+     *     tags={"poets"},
+     *     summary="Get all poets",
+     *     description="Getting all poets with pagination.",
+     *     operationId="getPoets",
+     *     @OA\Response(
+     *         response=200,
+     *         description="successful operation",
+     *     ),
+     *     @OA\Parameter(
+     *         name="per_page",
+     *         in="query",
+     *         required=false,
+     *         @OA\Schema(type="integer")
+     *     )
+     * )
+     */
     public function getPoets(PoetRequest $request): JsonResponse
     {
         $perPage = $request->input('per_page') ?? config('pagination.per_page.poets');
@@ -44,6 +43,32 @@ class PoetController extends Controller
      * @param GetPoetAction $action
      * @param $slug
      * @return PoetResource
+     *
+     * @OA\Get(
+     *      path="/api/poet/{slug}",
+     *      operationId="getPoetBySlug",
+     *      tags={"poet"},
+     *      summary="Get poet information",
+     *      description="Returns poet data",
+     *      @OA\Parameter(
+     *          name="slug",
+     *          description="Poet slug",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\JsonContent(ref="#/components/schemas/PoetResource")
+     *       ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Not found"
+     *      )
+     * )
      */
     public function show(GetPoetAction $action, $slug): PoetResource
     {
