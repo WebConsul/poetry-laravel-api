496650d91ec591b8ce0f52445ec2849e5d76f554
z-takaev <zatakata.web@gmail.com>
2022-10-13 00:20:28 +0300

    LEARN-POEMS-T-20 Create PoetResource

--- app/Http/Controllers/PoetPoemController.php
+++ app/Http/Controllers/PoetPoemController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PoemResource;
+use App\Models\Poem;
+use App\Models\Poet;
 use Illuminate\Http\Request;
 
 class PoetPoemController extends Controller
 {
-    public function show()
+    public function show(Poet $poet, Poem $poem)
     {
-        //
+        return new PoemResource($poem->load('poet', 'poet.poetData'));
     }
 }
--- app/Http/Resources/PoemResource.php
+++ app/Http/Resources/PoemResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\PoetData;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class PoemResource extends JsonResource
@@ -14,6 +15,13 @@ class PoemResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'language' => $this->language,
+            'title' => $this->title,
+            'created' => $this->created,
+            'slug' => $this->slug,
+
+            'poet' => new PoetResource($this->poet),
+        ];
     }
 }
